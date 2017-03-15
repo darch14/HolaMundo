a68c2d117e20124852e1c2fe5a1cbe0382d9a4ae
David Rodriguez <davidrch2013@gmail.com>
2017-03-14 21:12:01 -0500

    Aplicacion version 1.0

--- app/src/main/java/com/example/android/holamundo/Principal.java
+++ app/src/main/java/com/example/android/holamundo/Principal.java
@@ -1,11 +1,13 @@
 package com.example.android.holamundo;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Principal extends AppCompatActivity {
 
@@ -13,6 +15,7 @@ public class Principal extends AppCompatActivity {
     private EditText cajaApellido;
     private Intent i;
     private Bundle b;
+    private Resources res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +26,33 @@ public class Principal extends AppCompatActivity {
         cajaApellido = (EditText) findViewById(R.id.txtApellido);
         i = new Intent(this,Saludo.class);
         b = new Bundle();
+        res = this.getResources();
     }
 
     public void saludar(View v){
-        String nomb, apell;
-        nomb = cajaNombre.getText().toString();
-        apell = cajaApellido.getText().toString();
-        b.putString("Nombre", nomb);
-        b.putString("Apellido", apell);
+        if (validar()){
+            String nomb, apell;
+            nomb = cajaNombre.getText().toString();
+            apell = cajaApellido.getText().toString();
+            b.putString("Nombre", nomb);
+            b.putString("Apellido", apell);
 
-        i.putExtras(b);
+            i.putExtras(b);
 
-        startActivity(i);
+            startActivity(i);
+        }
+    }
+
+    public boolean validar(){
+        if (cajaNombre.getText().toString().isEmpty()){
+            cajaNombre.setError(res.getString(R.string.error_1));
+            //Toast.makeText(getApplicationContext(),"Digite por favor el nombre", Toast.LENGTH_SHORT.);
+            return false;
+        }
+        if (cajaApellido.getText().toString().isEmpty()){
+            cajaApellido.setError(res.getString(R.string.error_2));
+            return false;
+        }
+        return true;
     }
 }
